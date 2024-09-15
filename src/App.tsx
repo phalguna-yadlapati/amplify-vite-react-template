f020cd0d0f28701370ef56280514446ee6460d60
Phalu Yadlapati <v-pyadlapati@expediagroup.com>
2024-09-15 14:33:34 -0700

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -25,25 +25,26 @@ function App() {
 
   return (
       <Authenticator>
-          {({ signOut }) => (
+          {({ signOut, user }) => (
               <main>
-                  <h1>My todos</h1>
-                  <button onClick={createTodo}>+ new</button>
-                  <ul>
-                      {todos.map((todo) => (
-                          <li
-                              onClick={() => deleteTodo(todo.id)}
-                              key={todo.id}>{todo.content}</li>
-                      ))}
-                  </ul>
-                  <div>
-                      🥳 App successfully hosted. Try creating a new todo.
-                      <br/>
-                      <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                          Review next step of this tutorial.
-                      </a>
-                  </div>
-                  <button onClick={signOut}>Sign out</button>
+                <h1>My todos</h1>
+                <h1>{user?.signInDetails?.loginId}'s todos</h1>
+                <button onClick={createTodo}>+ new</button>
+                <ul>
+                  {todos.map((todo) => (
+                      <li
+                          onClick={() => deleteTodo(todo.id)}
+                          key={todo.id}>{todo.content}</li>
+                  ))}
+                </ul>
+                <div>
+                  🥳 App successfully hosted. Try creating a new todo.
+                  <br/>
+                  <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                    Review next step of this tutorial.
+                  </a>
+                </div>
+                <button onClick={signOut}>Sign out</button>
               </main>
           )}
       </Authenticator>
